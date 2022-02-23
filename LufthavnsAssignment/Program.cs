9cfb3e5f2d6ccb6b1b89d2c3979a0f19b53de0d8
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2022-02-23 12:42:43 +0100

    Initial commit

---
+++ LufthavnsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LufthavnsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AirportDBEntities AirportDBEntities = new AirportDBEntities();
+            Console.WriteLine("Airports:");
+            foreach (var item in AirportDBEntities.Airports)
+            {
+                Console.WriteLine($"IATA: {item.IATACode} | Airport Name: {item.Name} | CountryCode: {item.CountryCode} |  Country: {item.CountryName}");
+            }
+
+            Console.WriteLine("\nAirlines:");
+            foreach (var item in AirportDBEntities.Airlines)
+            {
+                Console.WriteLine($"Airline Name: {item.Name}");
+            }
+
+            Console.WriteLine("\nRoutes:");
+            foreach (var item in AirportDBEntities.Routes)
+            {
+                Console.WriteLine($"Airline Id: {item.Airline_Id} | Airport1: {item.PortOne} | Airport2: {item.PortTwo}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
